05ce87d7b5dafc7902e3ba89f9581ffe0e180e4f
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-12 12:49:14 +0300

    tilt logo skeleton setup

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <nav className='justify-end-m'>
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
       <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
     </nav>
   )
