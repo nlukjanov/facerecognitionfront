610027af2652d05536b4042fbafa88a8d68ca3c2
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-12 13:00:41 +0300

    added logo image and styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import face from './face.png'
 import './Logo.css';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shwdow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-        <div className="Tilt-inner"> Hello! </div>
+      <Tilt className="Tilt br2 shwdow-2" options={{ max : 55 }} style={{ height: 120, width: 120 }} >
+        <div className="Tilt-inner pa3"><img src={face} alt="face logo"/></div>
       </Tilt>
     </div>
   )
