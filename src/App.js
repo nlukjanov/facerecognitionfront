6b25100a737f018ba5a7838d51a8f1ceda0965aa
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-14 17:49:41 +0300

    added clarifai api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'c948170f77c746558d8b7b3d8e760a01'
+});
 
 const particlesOptions = {
   particles: {
@@ -33,6 +38,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+  );
   }
   render() {
     return (
