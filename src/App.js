aef48ba7299abff97b4593e73edf60dc05e212da
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-14 11:41:28 +0300

    added click and submit image buttons

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render() {
     return (
       <div className="App">
@@ -29,7 +43,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
         {/*<FaceRecognition />*/}
       </div>
     );
