4a0fdb38142981254879e588860cca0bcb0820a9
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-15 09:30:39 +0300

    full implementation of clarifai api for color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -29,23 +30,28 @@ class App extends Component {
     super()
     this.state = {
       input: '',
+      imageUrl: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        this.state.input)
+      .then(
       function(response) {
         console.log(response);
       },
       function(err) {
         // there was an error
       }
-  );
+    );
   }
   render() {
     return (
@@ -59,7 +65,7 @@ class App extends Component {
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}/>
-        {/*<FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
