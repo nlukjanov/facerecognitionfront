a51f75e5365beb88135bd991061b4174a3c19077
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-15 16:12:34 +0300

    implemented clarifai api and fix image size

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className="center">
-      <img src={imageUrl} alt=""/>
+    <div className="center ma">
+      <div className="absoulte mt2">
+        <img src={imageUrl} alt="" width='500px' height='auto'/>
+      </div>
     </div>
   )
 }
