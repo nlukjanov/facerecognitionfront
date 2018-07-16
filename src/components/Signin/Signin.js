f70d0bf38645bdc70e2976a6712ffff594a2a1e5
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-16 17:06:53 +0300

    added sign in out functionality

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Signin = () => {
+const Signin = ({ onRouteChange }) => {
   return (
     <article className="br3 ba shadow-5 b--black-10 mv6 w-100 w-50-m w-25-l mw6 center">
       <main className="pa4 black-80">
@@ -18,7 +18,12 @@ const Signin = () => {
             <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
           </fieldset>
           <div className="">
-            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+            <input
+              onClick={() => onRouteChange('home')}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Sign in"
+            />
           </div>
           <div className="lh-copy mt3">
             <a href="#0" className="f6 link dim black db">Register</a>
