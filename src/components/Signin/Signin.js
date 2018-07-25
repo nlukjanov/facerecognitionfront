64f08d28cbacf0fc135066f3de5de98fb8be18ff
Nikita Lukjanov <nlukjanov@gmail.com>
2018-07-25 11:35:53 +0100

    updated signin and register pages

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -4,15 +4,15 @@ const Signin = ({ onRouteChange }) => {
   return (
     <article className="br3 ba shadow-5 b--black-10 mv6 w-100 w-50-m w-25-l mw6 center">
       <main className="pa4 black-80">
-        <form className="measure">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">Password</label>
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
               <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
             </div>
             <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
@@ -29,7 +29,7 @@ const Signin = ({ onRouteChange }) => {
             <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             <a href='#0' className="f6 link dim black db">Forgot your password?</a>
           </div>
-        </form>
+        </div>
       </main>
     </article>
   )
